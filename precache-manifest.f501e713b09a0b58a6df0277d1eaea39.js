a9ac1af4d7e545815a30f824c917626247fbc295
unknown <alerpace@o2.pl>
2020-03-31 13:17:11 +0200

    Updates

---
+++ precache-manifest.f501e713b09a0b58a6df0277d1eaea39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64c0554640339bd8dced029bc73054d",
+    "url": "/generator-hasel/index.html"
+  },
+  {
+    "revision": "e7e27f8b4eabb1de535e",
+    "url": "/generator-hasel/static/css/main.17791af5.chunk.css"
+  },
+  {
+    "revision": "a1ae9f89a4380455723e",
+    "url": "/generator-hasel/static/js/2.11206db0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/generator-hasel/static/js/2.11206db0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7e27f8b4eabb1de535e",
+    "url": "/generator-hasel/static/js/main.d29e09c4.chunk.js"
+  },
+  {
+    "revision": "e8a7dc59ca611e328d35",
+    "url": "/generator-hasel/static/js/runtime-main.a4874982.js"
+  }
+]);
